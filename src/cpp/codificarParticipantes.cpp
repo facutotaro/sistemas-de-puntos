497e8f324c1a2dafbba587889c344530b0334126
Pablo Dallegri <7511563-Pablo-Dallegri@users.noreply.gitlab.com>
2021-09-03 13:30:07 +0000

    Acomodo indentación y saco índice redundante.

--- src/cpp/codificarParticipantes.cpp
+++ src/cpp/codificarParticipantes.cpp
@@ -12,53 +12,53 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	// Abrir archivos de entrada y salida
 	ifstream entrada;
-  ofstream salida, codigo;
+	ofstream salida, codigo;
 	entrada.open(argv[1]);
 	salida.open(argv[2]);
-  codigo.open(argv[3]);
+	codigo.open(argv[3]);
 
 	// Leer cantidad de participantes y de partidos, y copiar a la salida
 	int participantes, cantidadPartidos;
 	entrada >> participantes;
 	entrada >> cantidadPartidos;
-  salida << participantes << ' ' << cantidadPartidos << endl;
+	salida << participantes << ' ' << cantidadPartidos << endl;
 
-  // Inicializar diccionario de codificación
-  map<int, int> codigos = map<int, int>();
+	// Inicializar diccionario de codificación
+	map<int, int> codigos = map<int, int>();
 
 	// Procesar partidos
 	for (int i = 0; i < cantidadPartidos; i++) {
 		int fecha, equipoA, puntajeA, equipoB, puntajeB;
 
-    // Leer el partido sin codificar desde el archivo de entrada
+		// Leer el partido sin codificar desde el archivo de entrada
 		entrada >> fecha;
 		entrada >> equipoA;
 		entrada >> puntajeA;
 		entrada >> equipoB;
 		entrada >> puntajeB;
 
-    // En caso de encontrar un equipo por primera vez, generar su codificación
-    if (codigos.count(equipoA) == 0) {
-      codigos[equipoA] = codigos.size();
-      codigo << equipoA << ' ' << codigos[equipoA] << endl;
-    }
-    if (codigos.count(equipoB) == 0) {
-      codigos[equipoB] = codigos.size();
-      codigo << equipoB << ' ' << codigos[equipoB] << endl;
-    }
+		// En caso de encontrar un equipo por primera vez, generar su codificación
+		if (codigos.count(equipoA) == 0) {
+			codigos[equipoA] = codigos.size();
+			codigo << equipoA << endl;
+		}
+		if (codigos.count(equipoB) == 0) {
+			codigos[equipoB] = codigos.size();
+			codigo << equipoB << endl;
+		}
 
-    // Guardar el partido codificado en el archivo de salida
+		// Guardar el partido codificado en el archivo de salida
 		salida << fecha            << ' ';
-    salida << codigos[equipoA] << ' ';
-    salida << puntajeA         << ' ';
-    salida << codigos[equipoB] << ' ';
+		salida << codigos[equipoA] << ' ';
+		salida << puntajeA         << ' ';
+		salida << codigos[equipoB] << ' ';
 		salida << equipoB          << endl;
 	}
 
 	// Cerrar archivos
 	entrada.close();
-  salida.close();
-  codigo.close();
+	salida.close();
+	codigo.close();
 
 	return 0;
 }
